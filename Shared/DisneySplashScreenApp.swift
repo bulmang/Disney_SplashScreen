d44e9192c0019d8088a9997f62a5cb6f6a2f72e8
Kwan-123 <75381919+Kwan-123@users.noreply.github.com>
2022-03-22 16:29:40 +0900

    Initial Commit

---
+++ Shared/DisneySplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneySplashScreenApp.swift
+//  Shared
+//
+//  Created by 하명관 on 2022/03/22.
+//
+
+import SwiftUI
+
+@main
+struct DisneySplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
